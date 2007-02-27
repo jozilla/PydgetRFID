1ad336ae00c8cff43812d243ef41c0677993556a
Jo Vermeulen <jo.vermeulen@uhasselt.be>
2007-02-27 17:53:27 +0100

    Polished daemon by only emitting signals when a tag changes and improved exception handling.

--- daemon.py
+++ daemon.py
@@ -1,18 +1,24 @@
+#! /usr/bin/env python
+
 import dbus
 import dbus.service
 if getattr(dbus, 'version', (0,0,0)) >= (0,41,0):
     import dbus.glib # for signal handlers
 import gobject
+
 from threading import Thread
+import sys
 
 from hal.device_manager import DeviceManager
 from phidgets.phidget_rfid_reader import PhidgetRFIDReader
+from phidgets import TAG_NIL_VALUE
 
 class Daemon(dbus.service.Object):
     def __init__(self, bus_name, object_path="/Daemon"):
         dbus.service.Object.__init__(self, bus_name, object_path)
         self.setup()
         self.keep_reading = False
+        self.reading_thread = None
 
     def setup(self):
         self.dm = DeviceManager()
@@ -24,24 +30,46 @@ class Daemon(dbus.service.Object):
     @dbus.service.method('net.jozilla.PydgetRFID.DaemonInterface')
     def start_reading(self):
         self.keep_reading = True
+
         # start a new thread to handle the reading
         self.reading_thread = Thread(target=self.read)
         self.reading_thread.start()
 
     @dbus.service.method('net.jozilla.PydgetRFID.DaemonInterface')
     def stop_reading(self):
-        # stop the reading thread
         self.keep_reading = False
-        self.reading_thread.join() 
+
+        # stop the reading thread (if running)
+        if self.reading_thread != None:
+            self.reading_thread.join() 
+            self.reading_thread = None
 
     def read(self):
-        tag = self.dev.tag()
+        cnt = 0
+        prev_tag = self.dev.tag()
         
         while self.keep_reading:
+            # do a read
             self.dev.read_tag()
-            if tag != self.dev.tag():
-                tag = self.dev.tag()
-                self.tag_changed_signal(tag) # emit signal
+            cur_tag = self.dev.tag()
+
+            # Sometimes correct reads are alternated with nil reads.
+            # Therefore we only emit the signal when (1) two nil
+            # values are read the one after the other (meaning no RFID
+            # tag is near); or (2) when two different values are
+            # encountered after each other of which the last value is
+            # non-nil (meaning a different tag is read, either after a
+            # nil value or after another tag).
+
+            # two nil values
+            if prev_tag == cur_tag == TAG_NIL_VALUE:
+                self.tag_changed_signal(cur_tag) # emit signal
+                
+            # two different values of which the last is not nil
+            if prev_tag != cur_tag and cur_tag != TAG_NIL_VALUE: 
+                self.tag_changed_signal(cur_tag) # emit signal
+                
+            prev_tag = cur_tag 
 
     @dbus.service.signal('net.jozilla.PydgetRFID.DaemonInterface')
     def tag_changed_signal(self, tag):
@@ -49,16 +77,19 @@ class Daemon(dbus.service.Object):
         pass
 
 if __name__ == '__main__':
-    # needed to support threads
-    gobject.threads_init()
-    dbus.glib.init_threads()
-
-    # setup the DBUS service
-    session_bus = dbus.SessionBus()
-    name = dbus.service.BusName('net.jozilla.PydgetRFID.Daemon', bus=session_bus)
-    object = Daemon(name)
-
-    # run the mainloop
-    mainloop = gobject.MainLoop()
-    mainloop.run()
-
+    try:
+        # needed to support threads
+        gobject.threads_init()
+        dbus.glib.init_threads()
+    
+        # setup the DBUS service
+        session_bus = dbus.SessionBus()
+        name = dbus.service.BusName('net.jozilla.PydgetRFID.Daemon', bus=session_bus)
+        object = Daemon(name)
+    
+        # run the mainloop
+        mainloop = gobject.MainLoop()
+        mainloop.run()
+    except KeyboardInterrupt:
+        object.stop_reading()
+        sys.exit(1)
